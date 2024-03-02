befea4b5734866050425ebf90f92e56ff09e8d0a
daniel <daniel.blindr@gmail.com>
2024-03-02 18:49:32 +0200

    Lasers and rotation

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -1,6 +1,11 @@
 extends StaticBody2D
 
-@onready var laser_ray: RayCast2D = $LaserRay
+class_name Laser
+
+@onready var rotator: Node2D = $Rotator
+@onready var sprite: Polygon2D = $Rotator/Polygon2D
+@onready var laser_ray: RayCast2D = $Rotator/LaserRay
+
 var collision_point: Vector2:
 	set(value):
 		if collision_point != value:
@@ -8,10 +13,12 @@ var collision_point: Vector2:
 			queue_redraw()
 
 func _draw():
-	draw_line(laser_ray.position, collision_point, Color(1, 0, 0))
+	draw_line(laser_ray.position * rotator.transform.inverse(), collision_point, Color(0, 0, 1))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	if laser_ray.is_colliding():
 		var global_collision_point = laser_ray.get_collision_point()
 		collision_point = to_local(global_collision_point)
+
+func rotate_laser(radians: float):
+	rotator.rotate(radians)
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,8 +2,27 @@ extends CharacterBody2D
 
 const SPEED = 500.0
 
+@onready var influence: Area2D = $Influence
+var influencing_laser: Laser
+
+func _ready():
+	influence.connect("body_entered", _on_body_entered)
+	influence.connect("body_exited", _on_body_exited)
+
 func _physics_process(_delta):
 	var direction_horizontal = Input.get_axis("left", "right")
 	var direction_vertical = Input.get_axis("up", "down")
+	var laser_rotation = Input.get_axis("rotate_ccw", "rotate_cw")
+
 	velocity = Vector2(direction_horizontal, direction_vertical).normalized() * SPEED
+	if influencing_laser and laser_rotation:
+		influencing_laser.rotate_laser(laser_rotation * 0.01)
 	move_and_slide()
+
+func _on_body_entered(body: Node2D):
+	if body is Laser:
+		influencing_laser = body
+
+func _on_body_exited(body: Node2D):
+	if body == influencing_laser:
+		influencing_laser = null
